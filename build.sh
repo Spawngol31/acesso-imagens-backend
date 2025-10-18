05a932d88d4273f8188965b14661bb10b6fe525f
spawngol31 <jbq0101@gmail.com>
2025-10-18 20:52:01 -0300

    Adicionar comando de criar superuser ao build

--- build.sh
+++ build.sh
@@ -6,3 +6,10 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# --- NOVO COMANDO ADICIONADO ---
+# Tenta criar o super-utilizador de forma não-interativa.
+# Ele irá ler as 3 variáveis de ambiente que vamos configurar no Render.
+echo "A tentar criar o super-utilizador..."
+python manage.py createsuperuser --no-input 
+echo "Criação de super-utilizador concluída (pode ter falhado se já existir, o que é normal)."
